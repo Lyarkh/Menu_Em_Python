834ce4c09acc09a6e02986f0c1f89520584bfece
Lyarkh <lcemanuel.emanuel@gmail.com>
2022-02-16 17:41:45 -0300

    criando classe abstrata para opções

---
+++ src/classes/template_opcao.py
@@ -0,0 +1,11 @@
+from abc import ABCMeta, abstractmethod
+
+class TemplateOpcao(metaclass=ABCMeta):
+    
+    @abstractmethod
+    def __str__(self):
+        pass
+
+    @abstractmethod
+    def run(self):
+        pass
